0132a27f3220514862e690c10709139c57f65a43
Julian da Costa Faro <juliandacostafaro@hotmail.com>
2016-11-21 15:02:10 -0300

    Cambio mails de usuarios.

--- InscripcionCongreso/src/actions/CargarInscripcionesAction.java
+++ InscripcionCongreso/src/actions/CargarInscripcionesAction.java
@@ -15,10 +15,10 @@ public class CargarInscripcionesAction extends ActionSupport{
 		String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 		
 		trabajoService.guardarTrabajo("jorge", "caballero", "jorge_caballero@hotmail.com", "usuario1dssd@gmail.com", "titulo 1", Temas.BPM.name(), TiposPresentacion.CONFERENCIA.name(), null, lorem );
-		trabajoService.guardarTrabajo("juan", "ascacibar", "juan_ascacibar@hotmail.com", "usuariodssd3@gmail.com", "titulo 2", Temas.CLOUD.name(), TiposPresentacion.POSTER.name(), "juan/lunati,jorge/sanchez", lorem);
-		trabajoService.guardarTrabajo("rodrigo", "matheu", "rodrigo_matheu@hotmail.com", "usuario1dssd@gmail.com", "titulo 3", Temas.SOA.name(), TiposPresentacion.POSTER.name(), "miguel/castillo", lorem);
-		trabajoService.guardarTrabajo("marcos", "romero", "marcos_romero@hotmail.com", "usuariodssd3@gmail.com", "titulo 4", Temas.WS.name(), TiposPresentacion.CONFERENCIA.name(), "patricio/bolino,ignacio/vacca", lorem);
-		trabajoService.guardarTrabajo("ignacio", "castillo", "ignacio_castillo@hotmail.com", "usuario1dssd@gmail.com", "titulo 5", Temas.CLOUD.name(), TiposPresentacion.POSTER.name(), null, lorem);
+		trabajoService.guardarTrabajo("juan", "ascacibar", "juan_ascacibar@hotmail.com", "usuario2dssd@gmail.com", "titulo 2", Temas.CLOUD.name(), TiposPresentacion.POSTER.name(), "juan/lunati,jorge/sanchez", lorem);
+		trabajoService.guardarTrabajo("rodrigo", "matheu", "rodrigo_matheu@hotmail.com", "usuario3dssd@gmail.com", "titulo 3", Temas.SOA.name(), TiposPresentacion.POSTER.name(), "miguel/castillo", lorem);
+		trabajoService.guardarTrabajo("marcos", "romero", "marcos_romero@hotmail.com", "usuario4dssd@gmail.com", "titulo 4", Temas.WS.name(), TiposPresentacion.CONFERENCIA.name(), "patricio/bolino,ignacio/vacca", lorem);
+		trabajoService.guardarTrabajo("ignacio", "castillo", "ignacio_castillo@hotmail.com", "usuario5dssd@gmail.com", "titulo 5", Temas.CLOUD.name(), TiposPresentacion.POSTER.name(), null, lorem);
 		
 		return SUCCESS;
 	}
